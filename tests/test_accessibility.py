d9dd71130b2f83e9bf2572f343d0823e4a59d918
Kimberly Pennington <email@kimberly.codes>
2017-08-13 14:10:51 -0600

    added more accessibility tests

--- tests/test_accessibility.py
+++ tests/test_accessibility.py
@@ -67,3 +67,73 @@ class TestAccessibility:
             TestAccessibility.test_about_page_accessibility.__func__.__doc__ = violations[rule]['help']
 
         assert rule not in violations, report(violations[rule])
+
+    @pytest.mark.nondestructive
+    @pytest.mark.credentials
+    def test_settings_page_run_axe(self, base_url, selenium, vouched_user, axe, pytestconfig):
+        home_page = Home(base_url, selenium)
+        home_page.login(vouched_user['email'])
+        home_page.header.click_settings_menu_item()
+        pytestconfig.settings_page = axe.run()
+        assert pytestconfig.settings_page is not None
+
+    @pytest.mark.parametrize("rule", rules)
+    @pytest.mark.nondestructive
+    def test_settings_page_accessibility(self, pytestconfig, rule):
+        violations = pytestconfig.settings_page
+        if(rule in violations):
+            # Set docstring to description of current rule
+            TestAccessibility.test_settings_page_accessibility.__func__.__doc__ = violations[rule]['help']
+
+        assert rule not in violations, report(violations[rule])
+
+    @pytest.mark.nondestructive
+    @pytest.mark.credentials
+    def test_group_create_page_run_axe(self, base_url, selenium, vouched_user, axe, pytestconfig):
+        home_page = Home(base_url, selenium)
+        home_page.login(vouched_user['email'])
+
+        # Create a new group
+        group_name = str(uuid.uuid4())
+        settings = home_page.header.click_settings_menu_item()
+        settings.create_group(group_name)
+
+        pytestconfig.group_create_page = axe.run()
+
+        # New group data
+        new_group_description = 'This is an automated group.'
+        new_group_irc_channel = '#testgroup'
+
+        # Update the group description fields
+        group_description = group.description.description_info
+        group_description.set_description(new_group_description)
+        group_description.set_irc_channel(new_group_irc_channel)
+        group_description.click_update()
+
+        search_listings = home_page.header.search_for(group_name)
+        search_listings.open_group(group_name)
+
+        pytestconfig.group_search = axe.run()
+
+        assert pytestconfig.group_create_page is not None
+        assert pytestconfig.group_search is not None
+
+    @pytest.mark.parametrize("rule", rules)
+    @pytest.mark.nondestructive
+    def test_group_create_page_accessibility(self, pytestconfig, rule):
+        violations = pytestconfig.group_create_page
+        if(rule in violations):
+            # Set docstring to description of current rule
+            TestAccessibility.test_group_create_page_accessibility.__func__.__doc__ = violations[rule]['help']
+
+        assert rule not in violations, report(violations[rule])
+
+    @pytest.mark.nondestructive
+    @pytest.mark.credentials
+    def test_group_search_when_logged_in_accessibility(self, pytestconfig, rule):
+        violations = pytestconfig.group_search
+        if(rule in violations):
+            # Set docstring to description of current rule
+            TestAccessibility.test_group_create_page_accessibility.__func__.__doc__ = violations[rule]['help']
+
+        assert rule not in violations, report(violations[rule])
